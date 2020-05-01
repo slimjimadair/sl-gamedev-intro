1080a61f6817d0c110223aa48194dac582e70ceb
slimjimadair <slimjimadair@gmail.com>
2020-05-01 22:25:18 +0100

    Episode 9
    New project for local and global transforms.

---
+++ sl-igd-02-code/Assets/CubeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public Transform sphereTransform;
+
+    void Start()
+    {
+        sphereTransform.parent = transform;
+        sphereTransform.localScale = Vector3.one * 2;
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 180 * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.forward * 7 * Time.deltaTime, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sphereTransform.localPosition = Vector3.zero;
+        }
+    }
+}
