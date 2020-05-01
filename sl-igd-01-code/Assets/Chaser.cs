118505d691822aaeaadca726f774ba96af0de69d
slimjimadair <slimjimadair@gmail.com>
2020-05-01 19:03:40 +0100

    Episode 8
    Simple movement and player chasing.

---
+++ sl-igd-01-code/Assets/Chaser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+    public Transform targetTransform;
+    public float speed;
+
+    void Update()
+    {
+        Vector3 displacementFromTarget = targetTransform.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+        Vector3 velocity = directionToTarget * speed;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        float distanceToTarget = displacementFromTarget.magnitude;
+
+        if (distanceToTarget > 1.5)
+        {
+            transform.Translate(moveAmount);
+        }
+    }
+}
