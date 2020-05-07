eeb81226fb48727862c0db7892be89d1c5ecfc3b
slimjimadair <slimjimadair@gmail.com>
2020-05-07 20:56:37 +0100

    Episode 10b
    Completed collision detection and coin minigame.

---
+++ sl-igd-03-code/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody myRigidBody;
+    public float speed = 6;
+    Vector3 velocity;
+    int coinCount;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        velocity = direction * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidBody.position += velocity * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter(Collider triggerCollider)
+    {
+        if (triggerCollider.tag == "Coin")
+        {
+            Destroy(triggerCollider.gameObject);
+            coinCount += 1;
+            print(coinCount + " coins");
+        }
+        
+    }
+}
